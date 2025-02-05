b63723d910324c7d4cdffb48f87464e78e26c276
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-05 17:12:40 +0530

    Calculate length of the line by using point

---
+++ PointProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometryProject
+{
+    class PointProgarm
+    {
+        public double x { get; }
+        public double y { get; }
+
+        public PointProgarm(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class Line
+    {
+        public PointProgarm start { get; }
+        public PointProgarm end { get; }
+        public Line( PointProgarm start ,PointProgarm end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+
+        public double CalculateLength(){
+            double dx = start.x - end.x;
+            double dy = start.y - end.y;
+            return Math.Sqrt(dx * dx + dy * dy);
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using geometryProject;
+
+internal class Program
+{
+     static void Main(string[] args)
+     {
+        PointProgarm point1 = new PointProgarm(2, 3);
+        PointProgarm point2 = new PointProgarm(3, 4);
+
+        Line line = new Line(point1, point2);
+
+        double length = line.CalculateLength();
+        Console.WriteLine($"Length of the Line is = {length}"); 
+     }
+}
